44308d1c6ca5936be566be77769b110f5f540c5c
Vincent Pelletier <plr.vincent@gmail.com>
2013-01-19 23:38:16 +0100

    Implement an ITI1480A.spt replacement.

---
+++ CY7C68013A/device.c
@@ -0,0 +1,276 @@
+#include <fx2macros.h>
+#include <delay.h>
+#include <eputils.h>
+
+#define SYNCDELAY SYNCDELAY3
+#define CLKSPD48 bmCLKSPD1
+#define IFCFGFIFO (bmIFCFG0 | bmIFCFG1)
+#define bmSLCS bmBIT6
+#define bmEP1OUTBSY bmBIT1
+#define bmDYN_OUT bmBIT1
+#define bmENH_PKT bmBIT0
+#define TYPEBULK bmBIT5
+
+#define FPGA_nCONFIG bmBIT7
+#define FPGA_nSTATUS bmBIT6
+#define FPGA_CONF_DONE bmBIT5
+#define FPGA_DCLK bmBIT4
+
+#define CONFIG_UNCONFIGURED 0
+/* Device configuration compatible with ITI's firmware */
+#define CONFIG_COMPATIBLE 1
+
+#define COMMAND_FPGA 0
+#define COMMAND_STOP 1
+#define COMMAND_STATUS 2
+#define COMMAND_PAUSE 3
+
+#define COMMAND_FPGA_CONFIGURE_START 0
+#define COMMAND_FPGA_CONFIGURE_WRITE 1
+#define COMMAND_FPGA_CONFIGURE_STOP 2
+
+volatile BYTE config;
+
+//************************** Configuration Handlers *****************************
+
+// change to support as many interfaces as you need
+//volatile xdata BYTE interface=0;
+//volatile xdata BYTE alt=0; // alt interface
+
+// set *alt_ifc to the current alt interface for ifc
+BOOL handle_get_interface(BYTE ifc, BYTE* alt_ifc) {
+// *alt_ifc=alt;
+    return TRUE;
+}
+// return TRUE if you set the interface requested
+// NOTE this function should reconfigure and reset the endpoints
+// according to the interface descriptors you provided.
+BOOL handle_set_interface(BYTE ifc,BYTE alt_ifc) {
+    //interface=ifc;
+    //alt=alt_ifc;
+    return TRUE;
+}
+
+BYTE handle_get_configuration(void) {
+    return config;
+}
+
+BOOL handle_set_configuration(BYTE cfg) {
+    /* When changing configuration, use internal clock so we can configure
+    endpoints even if there is no clock on IFCLK input */
+    IFCONFIG = bmIFCLKSRC | bm3048MHZ;
+    SYNCDELAY;
+    REVCTL = bmDYN_OUT | bmENH_PKT; SYNCDELAY;
+    FIFORESET = bmNAKALL; SYNCDELAY;
+    FIFORESET = bmNAKALL | 2; SYNCDELAY;
+    FIFORESET = bmNAKALL | 4; SYNCDELAY;
+    FIFORESET = bmNAKALL | 6; SYNCDELAY;
+    FIFORESET = bmNAKALL | 8; SYNCDELAY;
+    switch (cfg) {
+        case CONFIG_UNCONFIGURED:
+            EP1OUTCFG &= ~bmVALID; SYNCDELAY;
+            EP1INCFG &= ~bmVALID; SYNCDELAY;
+            EP2CFG &= ~bmVALID; SYNCDELAY;
+            EP4CFG &= ~bmVALID; SYNCDELAY;
+            EP6CFG &= ~bmVALID; SYNCDELAY;
+            EP8CFG &= ~bmVALID; SYNCDELAY;
+            break;
+        case CONFIG_COMPATIBLE:
+            EP1OUTCFG = bmVALID | TYPEBULK; SYNCDELAY;
+            EP1INCFG = bmVALID | TYPEBULK; SYNCDELAY;
+            // XXX: syncdelay not required by spec, although required for
+            // similar regs.
+            EP1OUTBC = 0; SYNCDELAY;
+            EP2CFG = bmVALID | bmDIR | TYPEBULK; SYNCDELAY;
+            EP2FIFOCFG = bmAUTOIN | bmWORDWIDE; SYNCDELAY;
+            /* Autocommit 512B packets */
+            EP2AUTOINLENH = 2; SYNCDELAY;
+            EP2AUTOINLENL = 0; SYNCDELAY;
+            EP4CFG &= ~bmVALID; SYNCDELAY;
+            EP6CFG &= ~bmVALID; SYNCDELAY;
+            EP8CFG &= ~bmVALID; SYNCDELAY;
+            PINFLAGSAB = 0; SYNCDELAY;
+            PINFLAGSCD = 0; SYNCDELAY;
+            FIFOPINPOLAR = 0; SYNCDELAY;
+            break;
+        default:
+            return FALSE;
+    }
+    FIFORESET = 0; SYNCDELAY;
+    config = cfg;
+    return TRUE;
+}
+
+//******************* VENDOR COMMAND HANDLERS **************************
+
+BOOL handle_vendorcommand(BYTE cmd) {
+    return FALSE;
+}
+
+//********************  INIT ***********************
+
+void main_init(void) {
+    /* 1 CLKOUT: CLK0 23 */
+    CPUCS = CLKSPD48 | bmCLKOE;
+    /* Disable extra movx delays */
+    CKCON &= ~(bmBIT2 | bmBIT1 | bmBIT0);
+
+    /* PortA pinout:
+    INT0: TP14, 133
+    PA1: TP4, 119
+    SLOE: VCC
+    PA3: D1 ("Host power") led, then R7 and VCC - so on when low.
+    FIFOADR0:
+    FIFOADR1:
+    PKTEND:
+    SLCS#: GND
+    */
+    PORTACFG = bmSLCS | bmINT0;
+    IOA = bmBIT1;
+    OEA = bmBIT3 | bmBIT1;
+
+    /* PortE pinout:
+    108 PE0:             114
+    109 PE1:             113
+    110 PE2:             112
+    Used to load FPGA bitstream:
+    111 RXD0OUT:   DATA0  20
+    112 PE4 & TXD0: DCLK  21
+    113 PE5:   CONF_DONE 123
+    114 PE6:     STATUS# 121
+    115 PE7:     CONFIG#  26
+    */
+    PORTECFG = bmRXD0OUT;
+    IOE = bmBIT2 | bmBIT1 | bmBIT0;
+    OEE = FPGA_nCONFIG | FPGA_DCLK | bmBIT2 | bmBIT1 | bmBIT0;
+    /* SCON0 = XXXXX100: CLKOUT / 4, mode 0 */
+    SM2 = 1;
+
+    handle_set_configuration(CONFIG_UNCONFIGURED);
+}
+
+static inline void FPGAConfigureStart(void) {
+    /* Put FPGA into reset stage. */
+    /* Pull nCONFIG down */
+    IOE &= ~FPGA_nCONFIG;
+    /* Pull PE4 up to allow TXD0 signal to reach DCLK */
+    IOE |= FPGA_DCLK;
+    /* Wait for nSTATUS to become low */
+    while (IOE & FPGA_nSTATUS);
+    /* Pull nCONFIG up */
+    IOE |= FPGA_nCONFIG;
+    /* Arm TI to simulate a previously-completed transfer. */
+    TI = 1;
+    /* Wait for nSTATUS to become high */
+    while (!(IOE & FPGA_nSTATUS));
+}
+
+__sbit __at 0x98+1 TI_clear;
+static inline void FPGAConfigureWrite(__xdata unsigned char *buf, unsigned char len) {
+    /* Send len bytes from buf to FPGA. */
+    __idata unsigned char preloaded;
+    while (len) {
+        /* Do as much as possible before checking TI, to do something useful
+        instead of (maybe) just polling TI. */
+        len--;
+        preloaded = *buf++;
+        while (!TI);
+        /* Clear TI under an alias, so compiler doesn't think we are intending
+        to use a semaphore. Atomic "jbc-sjmp" is slower than "jnb-clr". */
+        TI_clear = 0;
+        SBUF0 = preloaded;
+    }
+}
+
+
+static inline void outPortC(unsigned char value, unsigned char ioe_mask) {
+    IOC = value;
+    OEC = 0xff;
+    IOE &= ~ioe_mask;
+    IOE |= ioe_mask;
+    OEC = 0;
+}
+
+static inline unsigned char inPortC(unsigned char ioe_mask) {
+    unsigned char result;
+    IOE &= ~ioe_mask;
+    result = IOC;
+    IOE |= ioe_mask;
+    return result;
+}
+
+static inline unsigned char FPGACommandRecv(void) {
+    outPortC(0x80, bmBIT0);
+    return inPortC(bmBIT1);
+}
+
+static inline void FPGACommandSend(unsigned char command) {
+    outPortC(0, bmBIT0);
+    outPortC(command, bmBIT2);
+}
+
+static inline void compatible_main_loop(void) {
+    if (!(EP01STAT & bmEP1OUTBSY)) {
+        if (EP1OUTBC == 64) {
+            switch (EP1OUTBUF[0]) {
+                case COMMAND_FPGA:
+                    switch (EP1OUTBUF[1]) {
+                        case COMMAND_FPGA_CONFIGURE_START:
+                            FPGAConfigureStart();
+                            break;
+                        case COMMAND_FPGA_CONFIGURE_WRITE:
+                            FPGAConfigureWrite(EP1OUTBUF + 2, EP1OUTBUF[63]);
+                            if (!(IOE & FPGA_nSTATUS)) {
+                                EP1OUTCS |= bmEPSTALL;
+                            }
+                            break;
+                        case COMMAND_FPGA_CONFIGURE_STOP:
+                            /* XXX: doesn't ensure the init stage is over.
+                            INIT_DONE pin is attached to PD6/FD4. Maybe it
+                            can be used ?
+                            If FPGA uses internal 10MHz clock for init, it
+                            takes 29.9us to finish configuration (and it
+                            probably does, CLKUSR doesn't seem connected to
+                            anything). */
+                            IOA &= ~bmBIT1;
+                            /* PortB pinout: FD[7:0]
+                               PortD pinout: FD[15:8]
+                            */
+                            IFCONFIG = IFCFGFIFO;
+                            /* XXX: assuming output clock is 48MHz */
+                            SYNCDELAY; RESETFIFOS();
+                            IOA |= bmBIT1;
+                            break;
+                        default:
+                            EP1OUTCS |= bmEPSTALL;
+                            break;
+                    }
+                    break;
+                case COMMAND_STOP:
+                    FPGACommandSend(1);
+                    break;
+                case COMMAND_STATUS:
+                    EP1INBUF[0] = 2;
+                    EP1INBUF[1] = FPGACommandRecv();
+                    SYNCDELAY; EP1INBC = 64;
+                    break;
+                case COMMAND_PAUSE:
+                    FPGACommandSend(EP1OUTBUF[1] ? 2 : 0);
+                    break;
+                default:
+                    EP1OUTCS |= bmEPSTALL;
+                    break;
+            }
+        }
+        SYNCDELAY;
+        EP1OUTBC = 0; SYNCDELAY;
+    }
+}
+
+void main_loop(void) {
+    switch(config) {
+        case CONFIG_COMPATIBLE:
+            compatible_main_loop();
+            break;
+    }
+}
---
+++ CY7C68013A/dscr.a51
@@ -0,0 +1,193 @@
+.module DEV_DSCR
+
+; descriptor types
+; same as setupdat.h
+DSCR_DEVICE_TYPE=1
+DSCR_CONFIG_TYPE=2
+DSCR_STRING_TYPE=3
+DSCR_INTERFACE_TYPE=4
+DSCR_ENDPOINT_TYPE=5
+DSCR_DEVQUAL_TYPE=6
+
+; for the repeating interfaces
+DSCR_INTERFACE_LEN=9
+DSCR_ENDPOINT_LEN=7
+
+; endpoint types
+ENDPOINT_TYPE_CONTROL=0
+ENDPOINT_TYPE_ISO=1
+ENDPOINT_TYPE_BULK=2
+ENDPOINT_TYPE_INT=3
+
+BCD_USB2 = 0x0002
+
+    .globl	_dev_dscr, _dev_qual_dscr, _highspd_dscr, _fullspd_dscr, _dev_strings
+; These need to be in code memory.  If
+; they aren't you'll have to manully copy them somewhere
+; in code memory otherwise SUDPTRH:L don't work right
+    .area	DSCR_AREA	(CODE)
+
+_dev_dscr:
+	.db	dev_dscr_end-_dev_dscr	; bLength
+	.db	DSCR_DEVICE_TYPE	; bDescriptorType
+	.dw	BCD_USB2		; bcdUSB
+	.db	0			; bDeviceClass
+	.db	0			; bDeviceSubClass
+	.db	0			; bDeviceProtocol
+	.db	64			; bMaxPacketSize0
+	.dw	0xc016			; idVendor
+	.dw	0xa907			; idProduct
+	.dw	0			; bcdDevice
+	.db	1			; iManufacturer
+	.db	2			; iProduct
+	.db	0			; iSerialNumber
+	.db	1			; bNumConfigurations
+dev_dscr_end:
+
+_dev_qual_dscr:
+	.db	dev_qualdscr_end-_dev_qual_dscr	; bLength
+	.db	DSCR_DEVQUAL_TYPE		; bDescriptorType
+	.dw	BCD_USB2			; bcdUSB
+	.db	0				; bDeviceClass
+	.db	0				; bDeviceSubClass
+	.db	0				; bDeviceProtocol
+	.db	64				; bMaxPacketSize0
+	.db	1				; bNumConfigurations
+	.db	0				; bReserved
+dev_qualdscr_end:
+
+_highspd_dscr:
+	.db	highspd_dscr_end-_highspd_dscr	; bLength
+	.db	DSCR_CONFIG_TYPE		; bDescriptorType
+	; wTotalLength (can't use .dw because byte order is different)
+	.db	(highspd_dscr_realend-_highspd_dscr) % 256
+	.db	(highspd_dscr_realend-_highspd_dscr) / 256
+	.db	1				; bNumInterfaces
+	.db	1				; bConfigurationValue
+	.db	0				; iConfiguration
+	.db	0x80				; bmAttributes
+	.db	0x32				; bMaxPower
+highspd_dscr_end:
+; all the interfaces next
+	.db	DSCR_INTERFACE_LEN	; bLength
+	.db	DSCR_INTERFACE_TYPE	; bDescriptorType
+	.db	0			; bInterfaceNumber
+	.db	0			; bAlternateSetting
+	.db	3			; bNumEndpoints
+	.db	0xff			; bInterfaceClass
+	.db	0			; bInterfaceSubClass
+	.db	0			; bInterfaceProtocol
+	.db	0			; iInterface
+; endpoint 1 out
+	.db	DSCR_ENDPOINT_LEN	; bLength
+	.db	DSCR_ENDPOINT_TYPE	; bDescriptorType
+	.db	0x01			; bEndpointAdress
+	.db	ENDPOINT_TYPE_BULK	; bmAttributes
+	.dw	0x4000			; wMaxPacketSize
+	.db	0x00			; bInterval
+; endpoint 1 in
+	.db	DSCR_ENDPOINT_LEN	; bLength
+	.db	DSCR_ENDPOINT_TYPE	; bDescriptorType
+	.db	0x81			; bEndpointAdress
+	.db	ENDPOINT_TYPE_BULK	; bmAttributes
+	.dw	0x4000			; wMaxPacketSize
+	.db	0x00			; bInterval
+; endpoint 2 in
+	.db	DSCR_ENDPOINT_LEN	; bLength
+	.db	DSCR_ENDPOINT_TYPE	; bDescriptorType
+	.db	0x82			; bEndpointAdress
+	.db	ENDPOINT_TYPE_BULK	; bmAttributes
+	.dw	0x0002			; wMaxPacketSize
+	.db	0x00			; bInterval
+highspd_dscr_realend:
+
+.even
+_fullspd_dscr:
+	.db	fullspd_dscr_end-_fullspd_dscr	; bLength
+	.db	DSCR_CONFIG_TYPE		; bDescriptorType
+	; wTotalLength (can't use .dw because byte order is different)
+	.db	(fullspd_dscr_realend-_fullspd_dscr) % 256
+	.db	(fullspd_dscr_realend-_fullspd_dscr) / 256
+	.db	1				; bNumInterfaces
+	.db	1				; bConfigurationValue
+	.db	0				; iConfiguration
+	.db	0x80				; bmAttributes
+	.db	0x32				; bMaxPower
+fullspd_dscr_end:
+; all the interfaces next
+	.db	DSCR_INTERFACE_LEN	; bLength
+	.db	DSCR_INTERFACE_TYPE	; bDescriptorType
+	.db	0			; bInterfaceNumber
+	.db	0			; bAlternateSetting
+	.db	0			; bNumEndpoints
+	.db	0			; bInterfaceClass
+	.db	0			; bInterfaceSubClass
+	.db	0			; bInterfaceProtocol
+	.db	0			; string index
+fullspd_dscr_realend:
+
+.even
+_dev_strings:
+_string0:
+	.db	string0end-_string0	; bLength
+	.db	DSCR_STRING_TYPE	; bDescriptorType
+	.db	0x09, 0x04		; wLANGID
+string0end:
+
+_string1: ; vendor
+	.db	string1end-_string1	; bLength
+	.db	DSCR_STRING_TYPE	; bDescriptorType
+	.db	'I, 0
+	.db	'n, 0
+	.db	't, 0
+	.db	'e, 0
+	.db	'r, 0
+	.db	'n, 0
+	.db	'a, 0
+	.db	't, 0
+	.db	'i, 0
+	.db	'o, 0
+	.db	'n, 0
+	.db	'a, 0
+	.db	'l, 0
+	.db	' , 0
+	.db	'T, 0
+	.db	'e, 0
+	.db	's, 0
+	.db	't, 0
+	.db	' , 0
+	.db	'I, 0
+	.db	'n, 0
+	.db	's, 0
+	.db	't, 0
+	.db	'r, 0
+	.db	'u, 0
+	.db	'm, 0
+	.db	'e, 0
+	.db	'n, 0
+	.db	't, 0
+	.db	's, 0
+string1end:
+
+_string2: ; device
+	.db	string2end-_string2	; bLength
+	.db	DSCR_STRING_TYPE	; bDescriptorType
+	.db	'U, 0
+	.db	'S, 0
+	.db	'B, 0
+	.db	' , 0
+	.db	'A, 0
+	.db	'n, 0
+	.db	'a, 0
+	.db	'l, 0
+	.db	'y, 0
+	.db	'z, 0
+	.db	'e, 0
+	.db	'r, 0
+	.db	' , 0
+	.db	'1, 0
+	.db	'4, 0
+	.db	'8, 0
+	.db	'0, 0
+	.db	'A, 0
+string2end:
